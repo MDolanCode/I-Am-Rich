278fc535a683a333bc0585a71ade6ac0a76794d9
Matthew Dolan <mdolan@rivian.com>
2020-01-05 10:58:14 -0800

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Matthew Dolan on 1/5/20.
+//  Copyright © 2020 Matthew Dolan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
